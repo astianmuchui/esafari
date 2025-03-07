e508e10c4c8e08467172aad5f94a3bdb389eae01
clencyc <clency2023@gmail.com>
2025-03-08 02:43:10 +0300

    updated street map view

--- routes/urls.py
+++ routes/urls.py
@@ -2,5 +2,7 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.home, name="Routes_Home"),
+    path('home/', views.home, name="Routes_Home"),
+    path('street/', views.streetmap_api, name="Routes_Streetmap_API"),
+
 ]
--- routes/views.py
+++ routes/views.py
@@ -1,7 +1,33 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+import json
 
 # Create your views here.
 
 def home(request):
     return render(request, "home.html")
+
+@csrf_exempt
+def streetmap_api(request):
+    if request.method == 'GET':
+        data = {
+            'message': 'Hello, this is a GET request!',
+            'status': 'success',
+        }
+        return JsonResponse(data)
+    elif request.method == 'POST':
+        try:
+            # Parse JSON data from the request body
+            data = json.loads(request.body)
+            response_data = {
+                'message': 'Hello, this is a POST request!',
+                'received_data': data,
+                'status': 'success',
+            }
+            return JsonResponse(response_data)
+        except json.JSONDecodeError:
+            return JsonResponse({'error': 'Invalid JSON'}, status=400)
+    else:
+        return JsonResponse({'error': 'Method not allowed'}, status=405)
+
